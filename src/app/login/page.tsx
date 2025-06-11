60997951695ec23ddb978b4f54111a48e83e3790
MatiasBidarte <matias.bidarte11@outlook.com>
2025-06-11 14:12:52 -0300

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,6 @@
 import React from "react";
 
 const Login = () => {
-  localStorage.setItem(
-    "usuario",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNsaWVudGVAZWplbXBsby5jb20iLCJvYnNlcnZhY2lvbmVzIjoiQ2xpZW50ZSBmcmVjdWVudGUiLCJkZXBhcnRhbWVudG8iOiJDYW5lbG9uZXMiLCJjaXVkYWQiOiJMYXMgUGllZHJhcyIsImRpcmVjY2lvbiI6IkF2LiBBcnRpZ2FzIDEyMyIsInRlbGVmb25vIjoiMDkyMTIzNDU2IiwiZGlzY3JpbWluYWRvciI6IlBlcnNvbmEiLCJub21icmUiOiJKdWFuIiwiYXBlbGxpZG8iOiJQJUMzJUIzcmV6IiwiaWF0IjoxNzE3NzU2Nzc2LCJleHAiOjE3MTc3NjAzNzZ9.0EbYkcJChJCR15FkgQqRFAs-oMEtnYHEYhdz5BsbnNw"
-  );
   return <div>Login</div>;
 };
 
