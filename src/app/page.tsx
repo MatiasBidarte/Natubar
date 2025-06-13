8b90e21603a7050627516e43f3ae14a1657c7e03
MatiasBidarte <matias.bidarte11@outlook.com>
2025-06-13 13:26:12 -0300

    registro terminado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,59 @@ import { useEffect } from "react";
 import { homemadeApple } from "./ui/fonts";
 
 export default function Home() {
-  const { products, fetchProducts, loading, error } = useProducts() as { products: Product[]; loading: boolean; error: string | null; fetchProducts: () => void };
-    useEffect(() => {
+  const { products, fetchProducts, loading, error } = useProducts() as {
+    products: Product[];
+    loading: boolean;
+    error: string | null;
+    fetchProducts: () => void;
+  };
+  useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
   return (
     <div className=" md:border-amber-600">
       <div className="portada flex items-center justify-center">
         <div className="text-center">
-           <h1 className={homemadeApple.className}>La felicidad en barra</h1>
-           <h2>NatuBar Barras Artesanales</h2>
-            <Button className="btn-portada">
-              Comprar ahora
-            </Button>
+          <h1 className={homemadeApple.className}>La felicidad en barra</h1>
+          <h2>NatuBar Barras Artesanales</h2>
+          <Button className="btn-portada">Comprar ahora</Button>
         </div>
       </div>
-        <Stack
-        spacing={{ xs: 1, sm: 2 ,}}
+      <Stack
+        spacing={{ xs: 1, sm: 2 }}
         direction="row"
         useFlexGap
-        sx={{ flexWrap: 'wrap', justifyContent: 'center' }}>
-                {loading ? (
-                  <>
-                    <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rounded" width={313.021} height={400} />
-                    <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rounded" width={313.021} height={400} />
-                    <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rounded" width={313.021} height={400} />
-                  </>
-                ) : (
-                  error ? (
-                    <p>Error: {error}</p>
-                  ) : (
-                    products.map((product: Product) => (
-                      <ProductCard key={product.id} product={product} />
-                    ))
-                  )
-                )}
-        
-        </Stack>     
+        sx={{ flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {loading ? (
+          <>
+            <Skeleton
+              sx={{ bgcolor: "grey.900" }}
+              variant="rounded"
+              width={313.021}
+              height={400}
+            />
+            <Skeleton
+              sx={{ bgcolor: "grey.900" }}
+              variant="rounded"
+              width={313.021}
+              height={400}
+            />
+            <Skeleton
+              sx={{ bgcolor: "grey.900" }}
+              variant="rounded"
+              width={313.021}
+              height={400}
+            />
+          </>
+        ) : error ? (
+          <p>Error: {error}</p>
+        ) : (
+          products.map((product: Product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        )}
+      </Stack>
     </div>
   );
 }
