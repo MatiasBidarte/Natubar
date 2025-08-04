c186af6b98b8e0efb45719c815bb25c544c7dcfe
MatiasBidarte <matias.bidarte11@outlook.com>
2025-08-04 15:38:21 -0300

    arreglo build

--- next.config.ts
+++ next.config.ts
@@ -7,19 +7,13 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.delishop.com.uy",
-        pathname: "/wp-content/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
-        pathname: "/id/**",
-      },
-      {
-        protocol: "https",
         hostname: "res.cloudinary.com",
       },
     ],
+  },
+  transpilePackages: ["lightningcss"],
+  experimental: {
+    optimizeCss: false,
   },
 };
 
