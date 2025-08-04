a6bc7f5b6b7fd457d93320326654484e57ba7a0a
MatiasBidarte <matias.bidarte11@outlook.com>
2025-08-04 15:27:49 -0300

    arreglo build

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -3,5 +3,5 @@ import { Roboto, Homemade_Apple } from "next/font/google";
 export const roboto = Roboto({ subsets: ["latin"] });
 export const homemadeApple = Homemade_Apple({
   subsets: ["latin"],
-  weight: ["400"],
+  weight: "400",
 });
